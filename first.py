95d536a0cfabaa878e3010746ee99dbb81e2c891
ssl40 <49948925+ssl40@users.noreply.github.com>
2019-04-24 17:31:02 +0300

    Update first.py

--- first.py
+++ first.py
@@ -1,9 +1,16 @@
+import pyowm
 import telebot
 
+owm = pyowm.OWM ("8111322c1d237a4929d7f80ed44865b6")
 bot = telebot.TeleBot ("805618208:AAEkp0Xat3eP3sFXGysRRU_aHTaBl_GFx7c")
 
 @bot.message_handler (content_types = ["text"])
 def send_echo (message):
-	bot.send_message (message.chat.id, message.text)
+	observation = owm.weather_at_place (message.text)
+	w = observation.get_weather ()
+	temp = w.get_temperature ("celsius")["temp"]
+
+	answer = w
+	answer += str(temp)
 
 bot.polling (none_stop = True)
