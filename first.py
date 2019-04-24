53e479702d68f2189f9e2d8a238d78439cbb8a55
ssl40 <49948925+ssl40@users.noreply.github.com>
2019-04-24 22:58:20 +0300

    Update first.py

--- first.py
+++ first.py
@@ -1,5 +1,9 @@
-import pyowm
 import telebot
+import pyowm
+
+from telebot import apihelper
+ 
+apihelper.proxy = {'https':'https://162.243.163.60:8080'}
 
 owm = pyowm.OWM ('40f2982c299cfd3cee2cc5ab4a749ff8')
 bot = telebot.TeleBot ("805618208:AAEkp0Xat3eP3sFXGysRRU_aHTaBl_GFx7c")
@@ -10,8 +14,8 @@ def send_echo (message):
 	w = observation.get_weather ()
 	temp = w.get_temperature ('celsius')["temp"]
 
-	answer = "Привет!"
-	answer += "str(temp)"
+	answer = "В городе " + message.text + " сейчас " + w.get_detailed_status() + ". Влажность воздуха " + str(hum) + "%." + "\n"
+	answer += "Температура " + str(temp) + "°С, скорость ветра " + str(wind) + " м/c."
 
 	bot.send.message (message.chat.id, answer)
 	
